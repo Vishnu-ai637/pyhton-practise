bd9ac7a3e668ea7819ba80a02251a5bb2decd8d3
vishnugowda <vishnugowda8242@gmail.com>
2026-09-02 23:38:11 +0530

    added two sum solution

--- boolean/comparistion operator.py
+++ boolean/comparistion operator.py
@@ -6,3 +6,18 @@
 a=20
 b=12
 print(a>b)
+print(a==b)
+
+
+
+#  practise coding
+nums=[2,5,7,5,8]
+target=9
+seen={}
+for index,num in enumerate(nums):
+    complement=target-num
+    if complement  in seen:
+        print(seen[complement],index)
+        break
+    else:
+        seen[num]=index
